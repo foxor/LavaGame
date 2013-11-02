6b280573f75971f17a1b2bc55405ec9d3290f4f2
Marty Loewenthal <mloewent@calpoly.edu>
2013-11-01 22:00:46 -0700

    added collision and map

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var moveSpeed = 4;
 var health = 5;
 var stgWidth = 320;
 var stgHeight = 320;
+var map = [];
 
 
 //02 Player Class
@@ -27,6 +28,9 @@ Player = Class.create(Sprite, {
     onenterframe: function() {
         
         //03 Player Controls
+        console.log(map[0][0]);
+        this.lastDirection[0] = 0;
+        this.lastDirection[1] = 0;
         if(game.input.left && !game.input.right && !game.input.up && !game.input.down){
             this.lastDirection[0] = -moveSpeed;
             this.lastDirection[1] = 0;
@@ -47,64 +51,65 @@ Player = Class.create(Sprite, {
             this.lastDirection[0] = 0;
             this.lastDirection[1] = 0;
         }
-
-        this.x += this.lastDirection[0];
-        this.y += this.lastDirection[1];
+        if (map[Math.floor(this.y / 16) - 1][Math.floor((this.x + this.lastDirection[0]) / 16)] != 0)
+            this.x += this.lastDirection[0];
+        if (map[Math.floor((this.y + this.lastDirection[1]) / 16)][Math.floor(this.x / 16)] != 0)
+            this.y += this.lastDirection[1];
         
         //04 Mouse Update
     }
 });
 
 //05 Gem Class
-Gem = Class.create(Sprite, {
-    initialize: function() {
-        Sprite.call(this, 16, 16);
-        this.image = game.assets['diamond-sheet.png'];
-    },
+// Gem = Class.create(Sprite, {
+//     initialize: function() {
+//         Sprite.call(this, 16, 16);
+//         this.image = game.assets['diamond-sheet.png'];
+//     },
 
-    onenterframe: function() {
+//     onenterframe: function() {
 
-        //Rotating using scaleX
+//         //Rotating using scaleX
         
-        //07 Collision Check
-    }
-});
+//         //07 Collision Check
+//     }
+// });
 
-//08 Bomb Class
-Bomb = Class.create(Sprite, {
-    initialize: function() {
-        Sprite.call(this, 16, 16);
-        this.image = game.assets['icon0.png'];
-        this.x = Math.random() * (stgWidth - 16);
-        this.y = Math.random() * (stgHeight - 16); //Account for the bottom part
-        if (this.y < 50) {
-            this.y = 50;
-        }
+// //08 Bomb Class
+// Bomb = Class.create(Sprite, {
+//     initialize: function() {
+//         Sprite.call(this, 16, 16);
+//         this.image = game.assets['icon0.png'];
+//         this.x = Math.random() * (stgWidth - 16);
+//         this.y = Math.random() * (stgHeight - 16); //Account for the bottom part
+//         if (this.y < 50) {
+//             this.y = 50;
+//         }
 
-        this.frame = 24;
-    },
+//         this.frame = 24;
+//     },
 
-    onenterframe: function() {
-        if (this.age === 60) {
-            game.rootScene.removeChild(this);
-        }
+//     onenterframe: function() {
+//         if (this.age === 60) {
+//             game.rootScene.removeChild(this);
+//         }
 
-        if (this.intersect(player)) {
-            player.health--;
-            game.rootScene.removeChild(this);
-            console.log("ouch!");
-        }
+//         if (this.intersect(player)) {
+//             player.health--;
+//             game.rootScene.removeChild(this);
+//             console.log("ouch!");
+//         }
 
-        if (this.age % 10 === 0) {
-            if (this.frame === 25) {
-                this.frame = 24;
-            } else {
-                this.frame++;
-            }
-        }
-    }
+//         if (this.age % 10 === 0) {
+//             if (this.frame === 25) {
+//                 this.frame = 24;
+//             } else {
+//                 this.frame++;
+//             }
+//         }
+//     }
 
-});
+// });
 
 
 //Begin game code
@@ -112,14 +117,43 @@ window.onload = function() {
     game = new Game(stgWidth, stgHeight);
     //Preload images
     //Any resources not preloaded will not appear
-    game.preload('icon0.png', 'diamond-sheet.png', 'bg.png');
+    game.preload('icon0.png', 'diamond-sheet.png', 'bg.png', 'Lavasmall.png');
 
     game.onload = function() { //Prepares the game
         //01 Add Background
-        bg = new Sprite(stgWidth, stgHeight);
-        bg.image = game.assets['bg.png'];
-        game.rootScene.addChild(bg);
-        //02 Add Player
+        map.push([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+        map.push([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+        map.push([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+        map.push([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+        map.push([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+        map.push([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+        map.push([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+        map.push([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+        map.push([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+        map.push([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+        map.push([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+        map.push([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+        map.push([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+        map.push([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+        map.push([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+        map.push([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+        map.push([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+        map.push([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+        map.push([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+        map.push([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]);
+        console.log(map);
+        for (var i = 0; i < 20; i++) {
+            for (var j = 0; j < 20; j++) {
+                bg = new Sprite(16, 16);
+                bg.image = game.assets['Lavasmall.png'];
+                var tile = map[i][j];
+                bg.frame = tile;
+                game.rootScene.addChild(bg);
+                bg.x = j * 16;
+                bg.y = i * 16;
+            }
+        }
+                //02 Add Player
         player = new Player();
         game.rootScene.addChild(player);
         //05 Add Gem
