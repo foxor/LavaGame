6f96825884e91b8c167389e6392cebeee276338c
kaironess <diana-phan@hotmail.com>
2013-11-02 16:18:07 -0700

    Update main.js

--- main.js
+++ main.js
@@ -45,7 +45,14 @@ function LoadLevel(level) {
         map.push("BBBBBBBBBBBBBBBBBBBB");
         map.push("BBBBBBBBBBBBBBBBBBBB");
         map.push("BBBBBBBBBBBBBBBBBBBB");
-        map.push([9, 0]);
+        map.push("BBBBBBBBBBBBBBBBBBBB");
+        map.push("BBBBBBBBBBBBBBBBBBBB");
+        map.push("BBBBBBBBBBBBBBBBBBBB");
+        map.push("BBBBBBBBBBBBBBBBBBBB");
+        map.push("BBBBBBBBBBBBBBBBBBBB");
+        map.push("BBBBBBBBBBBBBBBBBBBB");
+        map.push("BBBBBBBBBBBBBBBBBBBB");
+        map.push([0, 0]);
         player.x = 0 * 16;
         player.y = 9 * 16;
         break;
